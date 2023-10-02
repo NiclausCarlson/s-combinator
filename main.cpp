28dab6ba3ab83db659aa6faeae0bfd260405b0ed
NikclausCarlon <you@example.com>
2023-10-03 00:44:07 +0300

    fixed

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <functional>
 #include <string>
 
 template<typename T>
@@ -6,19 +7,17 @@ void Print(const T& expected, const T& actual){
     std::cout<<"Expected: "<<expected<<" Actual: "<<actual<<std::endl;
 }
 
-template<typename F, typename G, typename X>
-auto S(F f, G g, X x) {
-    return f(x)(g(x));
+template<typename A, typename B, typename C>
+A S(const std::function<A(B, C)>& f, const std::function<C(B)> & g, B x) {
+    return f(x, g(x));
 }
 
 void IntTest(){
-    auto f1 = [](int32_t x){
-        return [=](auto g_result){
-            return x + g_result;
-        };
+    std::function<int32_t(int32_t, int32_t)> f1 = [](int32_t x, int32_t g_result){
+        return x + g_result;
     };
 
-    auto g1 = [](int32_t x){
+    std::function<int32_t(int32_t)> g1 = [](int32_t x){
         return x + 1;
     };
     Print(3, S(f1, g1, 1));
@@ -26,28 +25,26 @@ void IntTest(){
 }
 
 void StringTest(){
-    auto f = [](std::string str){
-        return [=](auto g_result){
-            return str + g_result;
-        };
+    std::function<std::string(std::string, std::string)> f = [](std::string str, std::string g_result){
+        return str + g_result;
     };
-    auto g = [](std::string str){
+    std::function<std::string(std::string)> g = [](std::string str){
         return std::to_string(str.size());
     };
-    Print(std::string("abc3"), S(f, g, "abc"));
-    Print(std::string("cdef4"), S(f, g, "cdef"));
+    Print(std::string("abc3"), S(f, g, std::string("abc")));
+    Print(std::string("cdef4"), S(f, g, std::string("cdef")));
 }
 
 void HighFunctionTest() {
-    auto f = [](int32_t x){
-        return [=] (auto g){
-            return x + g(x);
-        };
+    using GG = std::function<int32_t(int32_t)>;
+    std::function<int32_t(int32_t, GG)> f = [](int32_t x, GG g){
+        return x + g(x);
     };
-    auto g = [](int32_t x){
-        return [=](int32_t y){
+    std::function<GG(int32_t)> g = [](int32_t x){
+        GG res = [=](int32_t y){
             return x + y;
         };
+        return res;
     };
     Print(3, S(f, g, 1));
     Print(15, S(f, g, 5));    
